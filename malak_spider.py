e451acf0c2af65fbdc41362a251c5936911e9425
spider261 <oauth.spider4@gmail.com>
2025-08-27 19:17:17 +0300

    Update malak_spider.py

--- malak_spider.py
+++ malak_spider.py
@@ -30,4 +30,4 @@ for i in range(720):
     data = {"content": random_emote, "type": "message"}
     res = requests.post(url, json=data, headers=headers)
     print(f"[{i+1}/180] Sent: {random_emote} | Status: {res.status_code}")
-    time.sleep(10)
+    time.sleep(9)
